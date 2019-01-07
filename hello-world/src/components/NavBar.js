c1ba56295ecbb9a3eebc1b214a49ed31795b7357
tmarsh1245 <tmarsh@umass.edu>
2019-01-07 09:55:02 -0500

    Following tutorial

---
+++ hello-world/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default() =>
+    <nav className= "navbar" role= "navigation">
+        <div className= "navbar-brand">
+            <a className="navbar-item is-primary">
+                Tanner's Site Idiot
+            </a>
+        </div>
+    </nav>
+
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,4 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import NavBar from "../components/NavBar"
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{color: 'blue'}}>
+    <Link to= "/contact/">Contact</Link>
+    <Header headerText = "Whats up Fools"/>
+    <p>why oh why</p>
+    <img src={"https://source.unsplash.com/random/400x200"} alt={""} />
+</div>
+
 
